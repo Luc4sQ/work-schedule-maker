cf027dc18bd23706946f2bb781adba811c1ca834
Luc4s <pq164536@uni-greifswald.de>
2022-10-25 21:09:38 +0200

    slight changes

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { employee, week, plan } from './employee/workStructure';
+
+let x: week = {days: 3, timePerUnit: 4};
+
+let s: string = x.days.toString();
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +16,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <p>
+      {s}
+    </p>
   </React.StrictMode>
 );
 
